2cff9ed0d2dc28a63097097ff4cd4139a5f2d97e
Shashankahir <120783869+Shashankahir@users.noreply.github.com>
2023-01-19 19:39:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Yash_Yadav99/PrimeflixCinemaBot.git /PrimflixCinemaBot
+cd /PrimflixCinemaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
